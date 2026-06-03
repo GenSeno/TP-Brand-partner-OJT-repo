85dce6154680f426e54477e38d83d3a7310ab3e5
Jomprii <jyamson30@gmail.com>
2026-06-03 19:28:18 +0800

    updates

--- routes/store.php
+++ routes/store.php
@@ -81,13 +81,6 @@ Route::group([
         ->name('brand-partner.account')
         ->middleware('auth');
 
-    //Reset Password
-    // Route::get('/reset-password/{token}', function (string $token) {
-    //     return Inertia::render('store/auth/reset-password', [
-    //         'token' => $token,]);
-    //         })->name('password.reset');
-
-
     // Update user profile
     Route::patch('/account/profile', [AuthController::class, 'updateProfile'])
         ->name('brand-partner.account.update')
